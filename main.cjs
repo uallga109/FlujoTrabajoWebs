2d4267412da7748377d90144103bdc1d1151414f
García Aguilera Luis <uallga109@inlumine.ual.es>
2026-04-20 13:48:19 +0200

    Lanzamiento 1.0.1

--- main.cjs
+++ main.cjs
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path'); // <- Necesario para buscar archivos locales
+const { autoUpdater } = require('electron-updater'); // <- El radar modo Pro
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -16,7 +17,19 @@ function createWindow() {
   }
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  // Si el programa está instalado (.exe), busca actualizaciones en GitHub en silencio
+  if (app.isPackaged) {
+    autoUpdater.checkForUpdatesAndNotify();
+  }
+});
+
+// Chivatos de consola (por si quieres comprobar si funciona internamente)
+autoUpdater.on('checking-for-update', () => console.log('Buscando actualizaciones...'));
+autoUpdater.on('update-available', () => console.log('¡Hay una nueva versión! Descargando...'));
+autoUpdater.on('update-downloaded', () => console.log('Descarga lista. Se instalará al cerrar la app.'));
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
